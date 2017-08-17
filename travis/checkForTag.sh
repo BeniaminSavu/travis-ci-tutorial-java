161442b22551fca971e632ed95217ee045632f99
Beniamin Savu <benisavu@gmail.com>
2017-08-17 15:31:23 +0300

    change

--- travis/checkForTag.sh
+++ travis/checkForTag.sh
@@ -1,3 +1,4 @@
 if [[ ! -z ${TRAVIS_TAG} ]] 
 then  mvn --batch-mode release:update-versions -DdevelopmentVersion=${TRAVIS_TAG};
+echo "The tah is -------------------------------- ${TRAVIS_TAG}";
 fi
