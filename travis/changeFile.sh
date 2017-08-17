08891f5c14d40807fc6d71d37436db218c144109
BeniaminSavu <benisavu@gmail.com>
2017-08-17 13:57:22 +0300

    test 2

--- travis/changeFile.sh
+++ travis/changeFile.sh
@@ -1,20 +1,11 @@
 rev=$(git rev-parse --short HEAD)
-
-cd out
-
-git init
 git config user.name "BeniaminSavu"
 git config user.email "benisavu@gmail.com"
-
-git remote add origin "https://${GH_TOKEN}@github.com/${TRAVIS_REPO_SLUG}.git"
 git reset
-
 msg="Tag Generated from TravisCI for build $TRAVIS_BUILD_NUMBER"
 echo "$msg" >> build.txt
-
 ls
 git status
-
 git add build.txt
 git commit -m "rebuild pages at ${rev}"
 git push -v
