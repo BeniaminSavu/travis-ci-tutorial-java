15c9f6e6bef0890fbc7fb7db31c4d5b7f5ff219f
BeniaminSavu <benisavu@gmail.com>
2017-08-17 13:21:55 +0300

    Update changeFile.sh

--- travis/changeFile.sh
+++ travis/changeFile.sh
@@ -1,8 +1,25 @@
-git remote set-url origin https://${GH_TOKEN}@github.com/${TRAVIS_REPO_SLUG}.git
-git config user.name "${GIT_NAME}"
-git config user.email "${GIT_EMAIL}"
+rev=$(git rev-parse --short HEAD)
+
+REPONAME=`basename $PWD`
+PARENTDIR=`dirname $PWD`
+USERNAME=`basename $PARENTDIR`
+
+cd out
+
+git init
+git config user.name "Beniamin Savu"
+git config user.email "benisavu@gmail.com"
+
+git remote add upstream "https://$GH_TOKEN@github.com/$USERNAME/$REPONAME.git"
+git fetch upstream
+git reset upstream/master
+
 msg="Tag Generated from TravisCI for build $TRAVIS_BUILD_NUMBER"
 echo "$msg" >> build.txt
-git add build.txt
-git commit -m "Update build version file with $TRAVIS_BUILD_NUMBER"
-git push origin master
+
+ls
+git status
+
+git add -A .
+git commit -m "rebuild pages at ${rev}"
+git push -q upstream HEAD:master
