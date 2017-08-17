f16d6cca7bcfedc2fd905b956c2415547ba90f8b
BeniaminSavu <benisavu@gmail.com>
2017-08-17 14:21:51 +0300

    test 7

--- travis/changeFile.sh
+++ travis/changeFile.sh
@@ -1,13 +1,12 @@
-rev=$(git rev-parse --short HEAD);
 git config user.name "BeniaminSavu";
 git config user.email "benisavu@gmail.com";
-git fetch
-git checkout test
+git fetch;
+git checkout master;
 git reset;
 msg="Tag Generated from TravisCI for build $TRAVIS_BUILD_NUMBER";
 echo "$msg" >> build.txt;
 git status;
 git add build.txt;
 git status;
-git commit -m "rebuild pages at ${rev}";
+git commit -m "new commit";
 git push origin HEAD:master;
