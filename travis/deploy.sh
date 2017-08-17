34bde0bd5d359f4a7cf9f1a9448a91790e0f7b68
BeniaminSavu <benisavu@gmail.com>
2017-08-17 12:12:32 +0300

    Update deploy.sh

--- travis/deploy.sh
+++ travis/deploy.sh
@@ -2,9 +2,5 @@ YEAR=$(date +"%Y")
 MONTH=$(date +"%m")
 git config --global user.email "${GIT_EMAIL}"
 git config --global user.name "${GIT_NAME}"
-git config --global user.password ${GH_PASSWORD}
-git config --global push.default simple
-git remote add origin https://${GH_TOKEN}@github.com/${TRAVIS_REPO_SLUG}.git
 msg="Tag Generated from TravisCI for build $TRAVIS_BUILD_NUMBER"
 git push origin master && git push origin master --tags
-ls -a
