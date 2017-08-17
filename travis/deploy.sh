d8a45948381a721c53fc4e307a571e610f2d54e0
BeniaminSavu <benisavu@gmail.com>
2017-08-17 11:42:07 +0300

    Update deploy.sh

--- travis/deploy.sh
+++ travis/deploy.sh
@@ -6,10 +6,6 @@ git config --global push.default simple
 export GIT_TAG=V2.$YEAR-$MONTH.$TRAVIS_BUILD_NUMBER
 git fetch --tags
 msg="Tag Generated from TravisCI for build $TRAVIS_BUILD_NUMBER"
-echo Git Tag $GIT_TAG and $TRAVIS_BUILD_NUMBER
-if git tag $GIT_TAG -a -m "$msg" 2>/dev/null; then
-git tag $GIT_TAG -a -m "Tag Generated from TravisCI for build $TRAVIS_BUILD_NUMBER"
+echo "Git Tag $GIT_TAG and $TRAVIS_BUILD_NUMBER"
 git push origin master && git push origin master --tags
 ls -a
-else echo Tag already exists! 
-fi
