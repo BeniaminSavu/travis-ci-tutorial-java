67f5df98436ad6604b93dc04d3d5502462e4468d
BeniaminSavu <benisavu@gmail.com>
2017-08-17 11:23:21 +0300

    Update deploy.sh

--- travis/deploy.sh
+++ travis/deploy.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 YEAR=$(date +"%Y")
 MONTH=$(date +"%m")
 git config --global user.email "${GIT_EMAIL}"
@@ -7,8 +9,9 @@ git remote add origin https://${GH_TOKEN}@github.com/${TRAVIS_REPO_SLUG}.git
 export GIT_TAG=V2.$YEAR-$MONTH.$TRAVIS_BUILD_NUMBER
 git fetch --tags
 msg="Tag Generated from TravisCI for build $TRAVIS_BUILD_NUMBER"
-if git tag $GIT_TAG -a -m "$msg" 2>/dev/null; then
+if git tag $GIT_TAG -a -m "$msg" >/dev/null; then
 git tag $GIT_TAG -a -m "Tag Generated from TravisCI for build $TRAVIS_BUILD_NUMBER"
 git push origin master && git push origin master --tags
 ls -aR
-else echo Tag already exists! fi
+else echo Tag already exists! 
+fi
