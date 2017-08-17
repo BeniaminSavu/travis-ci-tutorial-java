c89420ce04b243cc1f5a625e58c86fbf035205f1
BeniaminSavu <benisavu@gmail.com>
2017-08-17 12:33:48 +0300

    Update changeFile.sh

--- travis/changeFile.sh
+++ travis/changeFile.sh
@@ -1,21 +1,8 @@
-SOURCE_BRANCH="master"
-TARGET_BRANCH="master"
-
-REPO=`git config remote.origin.url`
-SSH_REPO=${REPO/https:\/\/github.com\//git@github.com:}
-SHA=`git rev-parse --verify HEAD`
-
-git clone $REPO out
-# Clean out existing contents
-rm -rf out/**/* || exit 0
-
-cd out
 git config user.name "${GIT_NAME}"
 git config user.email "${GIT_EMAIL}"
 
 msg="Tag Generated from TravisCI for build $TRAVIS_BUILD_NUMBER"
 echo "$msg" >> build.txt
 git add build.txt
-git status
 git commit -m "Update build version file with $TRAVIS_BUILD_NUMBER"
-git push $SSH_REPO $TARGET_BRANCH
+git push origin master
