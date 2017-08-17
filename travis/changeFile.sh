5963999f667186d289ddf49dd9aec9b7ef1bd72b
BeniaminSavu <benisavu@gmail.com>
2017-08-17 14:08:46 +0300

    checkout

--- travis/changeFile.sh
+++ travis/changeFile.sh
@@ -1,6 +1,7 @@
 rev=$(git rev-parse --short HEAD);
 git config user.name "BeniaminSavu";
 git config user.email "benisavu@gmail.com";
+git checkout master
 git reset;
 msg="Tag Generated from TravisCI for build $TRAVIS_BUILD_NUMBER";
 echo "$msg" >> build.txt;
